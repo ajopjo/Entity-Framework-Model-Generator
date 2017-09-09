c1b96b06a5865dada306aa91957b40c93873a598
ajo.john <ajopjo@rediffmail.com>
2017-09-09 08:46:36 -0500

    VS 2017 upgrade

--- ProjectExtensions.cs
+++ ProjectExtensions.cs
@@ -3,13 +3,10 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace EfModelGenerator
